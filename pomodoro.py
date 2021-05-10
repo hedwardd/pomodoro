5c95b48962c1c9f5408865d8aaa016470561460a
Heath Daniel <mr.heath.daniel@gmail.com>
2021-05-10 19:12:46 -0400

    Fix bug preventing break pausing

--- pomodoro.py
+++ pomodoro.py
@@ -15,10 +15,10 @@ class PomodoroApp(object):
             "stop_break": "Stop Break",
             "break_end_message": "Break is up. Time to get back to it :)",
             "halfway_message": "Halfway there!",
-            # "interval": 1800,
-            "interval": 10,
-            # "break_interval": 600
-            "break_interval": 8
+            "interval": 1800,
+            "break_interval": 600,
+            # "interval": 10,
+            # "break_interval": 8,
         }
 
         # Rumps Objects
@@ -32,6 +32,7 @@ class PomodoroApp(object):
             title=self.config["start_break"], callback=self.start_timer)
         self.stop_button = rumps.MenuItem(
             title=self.config["stop"], callback=None)
+
         self.break_submenu = rumps.MenuItem(title=self.config["break_menu"])
         self.break_submenu.add(self.start_pause_break)
 
@@ -49,11 +50,15 @@ class PomodoroApp(object):
         self.timer.stop()
         self.timer.count = 0
         self.app.title = "🍅"
+        self.stop_button.set_callback(None)
+        self.start_pause_timer.title = self.config["start"]
+        self.start_pause_break.title = self.config["start_break"]
 
     def on_tick(self, sender):
         time_left = sender.end - sender.count
-        mins = time_left // 60 if time_left >= 0 else time_left // 60 + 1
+        mins = time_left // 60 if time_left >= 0 else (-1 * time_left) // 60
         secs = time_left % 60 if time_left >= 0 else (-1 * time_left) % 60
+        is_break = True if sender.end != sender.interval else False
         if time_left == 0:
             # Send different notification if on break
             noti_message = self.config["timer_end_message"] if not self.is_break else self.config["break_end_message"]
@@ -61,42 +66,42 @@ class PomodoroApp(object):
                 title=self.config["app_name"],
                 subtitle=noti_message,
                 message='')
-        else:
-            # Don't send halfway notification if on break
-            if sender.count > 0 and sender.end / sender.count == 2 and not self.is_break:
-                rumps.notification(
-                    title=self.config["app_name"],
-                    subtitle=self.config["halfway_message"],
-                    message='')
-            formatted_time = '{:2d}:{:02d}'.format(mins, secs) if time_left >= 0 else '({:2d}:{:02d} )'.format(mins, secs) 
-            self.app.title = formatted_time if not self.is_break else "🧘‍♂️" + formatted_time + "🧘‍♀️"
+        # Don't send halfway notification if on break
+        elif sender.count > 0 and sender.end / sender.count == 2 and not self.is_break:
+            rumps.notification(
+                title=self.config["app_name"],
+                subtitle=self.config["halfway_message"],
+                message='')
+        formatted_time = '{:2d}:{:02d}'.format(mins, secs) if time_left >= 0 else '(+{:2d}:{:02d} )'.format(mins, secs) 
+        self.app.title = formatted_time if not self.is_break else "🧘‍♂️" + formatted_time + "🧘‍♀️"
         sender.count += 1
 
     # TODO: Separate button text updates from timer update. I.e., separate actions
     def start_timer(self, sender):
         self.stop_button.set_callback(self.stop_timer)
         if sender.title == self.config["start"] or sender.title == self.config["continue"]:
+            self.is_break = False
             if sender.title == self.config["start"]:
                 self.timer.count = 0
                 self.timer.end = self.interval
-                self.is_break = False
             sender.title = self.config["pause"]
             self.timer.start()
         elif sender.title == self.config["start_break"] or sender.title == self.config["continue_break"]:
+            self.is_break = True
             if sender.title == self.config["start_break"]:
                 self.timer.count = 0
                 self.timer.end = self.break_interval
-                self.is_break = True
             sender.title = self.config["pause_break"]
             self.timer.start()
-        else:
+        elif sender.title == self.config["pause"]:
             sender.title = self.config["continue"]
+            self.timer.stop()
+        elif sender.title == self.config["pause_break"]:
+            sender.title = self.config["continue_break"]
             self.timer.stop()
 
     def stop_timer(self, sender):
         self.set_up_menu()
-        self.stop_button.set_callback(None)
-        self.start_pause_timer.title = self.config["start"]
 
     def run(self):
         self.app.run()
