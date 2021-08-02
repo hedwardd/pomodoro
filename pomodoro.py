e46c870439e2428c02e3512b7df04012cac4a997
Heath Daniel <mr.heath.daniel@gmail.com>
2021-08-02 13:38:51 -0400

    Add more duration options

--- pomodoro.py
+++ pomodoro.py
@@ -3,20 +3,24 @@ class PomodoroApp(object):
     def __init__(self):
         self.config = {
             "app_name": "Pomodoro Remix",
-            "start": "Start Timer",
+            "start": "Start 30m Timer",
+            "start2": "Start 15m Timer",
             "pause": "Pause Timer",
             "continue": "Continue Timer",
             "stop": "Stop Timer",
             "timer_end_message": "Time is up! Take a break :)",
             "break_menu": "Break",
-            "start_break": "Start Break",
+            "start_break": "Start 10m Break",
+            "start_break2": "Start 5m Break",
             "pause_break": "Pause Break",
             "continue_break": "Continue Break",
             "stop_break": "Stop Break",
             "break_end_message": "Break is up. Time to get back to it :)",
             "halfway_message": "Halfway there!",
             "interval": 1800,
+            "interval2": 900,
             "break_interval": 600,
+            "break_interval2": 300,
             # "interval": 10,
             # "break_interval": 8,
         }
@@ -28,19 +32,26 @@ class PomodoroApp(object):
         # Menu Setup
         self.start_pause_timer = rumps.MenuItem(
             title=self.config["start"], callback=self.start_timer)
+        self.start_pause_timer2 = rumps.MenuItem(
+            title=self.config["start2"], callback=self.start_timer)
         self.start_pause_break = rumps.MenuItem(
             title=self.config["start_break"], callback=self.start_timer)
+        self.start_pause_break2 = rumps.MenuItem(
+            title=self.config["start_break2"], callback=self.start_timer)
         self.stop_button = rumps.MenuItem(
             title=self.config["stop"], callback=None)
 
         self.break_submenu = rumps.MenuItem(title=self.config["break_menu"])
         self.break_submenu.add(self.start_pause_break)
+        self.break_submenu.add(self.start_pause_break2)
 
-        self.app.menu = [self.start_pause_timer, self.stop_button, self.break_submenu]
+        self.app.menu = [self.start_pause_timer, self.start_pause_timer2, self.stop_button, self.break_submenu]
 
         # Values/Attributes
         self.interval = self.config["interval"]
+        self.interval2 = self.config["interval2"]
         self.break_interval = self.config["break_interval"]
+        self.break_interval2 = self.config["break_interval2"]
         self.is_break = False
 
         # Initiate
@@ -52,7 +63,9 @@ class PomodoroApp(object):
         self.app.title = "🍅"
         self.stop_button.set_callback(None)
         self.start_pause_timer.title = self.config["start"]
+        self.start_pause_timer2.title = self.config["start2"]
         self.start_pause_break.title = self.config["start_break"]
+        self.start_pause_break2.title = self.config["start_break2"]
 
     def on_tick(self, sender):
         time_left = sender.end - sender.count
@@ -79,18 +92,24 @@ class PomodoroApp(object):
     # TODO: Separate button text updates from timer update. I.e., separate actions
     def start_timer(self, sender):
         self.stop_button.set_callback(self.stop_timer)
-        if sender.title == self.config["start"] or sender.title == self.config["continue"]:
+        if sender.title == self.config["start"] or sender.title == self.config["start2"] or sender.title == self.config["continue"]:
             self.is_break = False
             if sender.title == self.config["start"]:
                 self.timer.count = 0
                 self.timer.end = self.interval
+            elif sender.title == self.config["start2"]:
+                self.timer.count = 0
+                self.timer.end = self.interval2
             sender.title = self.config["pause"]
             self.timer.start()
-        elif sender.title == self.config["start_break"] or sender.title == self.config["continue_break"]:
+        elif sender.title == self.config["start_break"] or sender.title == self.config["start_break2"] or sender.title == self.config["continue_break"]:
             self.is_break = True
             if sender.title == self.config["start_break"]:
                 self.timer.count = 0
                 self.timer.end = self.break_interval
+            elif sender.title == self.config["start_break2"]:
+                self.timer.count = 0
+                self.timer.end = self.break_interval2
             sender.title = self.config["pause_break"]
             self.timer.start()
         elif sender.title == self.config["pause"]:
